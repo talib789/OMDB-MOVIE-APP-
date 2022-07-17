33e6e0a6e83afcbdbca40db6db9b44b5343d8207
Md Talib <95065465+talib789@users.noreply.github.com>
2022-07-17 09:29:12 +0530

    Add files via upload

---
+++ OMDB MOVIE APP/scripts/data.js
@@ -0,0 +1,10 @@
+let imageData = [
+    "./Banner Img/1.png",
+    "./Banner Img/2.png",
+    "./Banner Img/3.png",
+    "./Banner Img/4.png",
+    "./Banner Img/5.png",
+    "./Banner Img/6.png"];
+ 
+    localStorage.setItem("images", JSON.stringify(imageData));
+ 
